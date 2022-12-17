5335c854fdd3502b40045423dc8fc381df519dc3
Saransh Kumar <ksaransh682@gmail.com>
2022-12-18 01:08:45 +0530

    Save new user detail in the db

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
@@ -13,6 +14,26 @@ class AuthController extends Controller
     {
         $inputs = $request->validated();
 
-        dd("here");
+        $user = User::create([
+            'first_name' => $inputs['first_name'],
+            'last_name' => $inputs['last_name'],
+            'user_name' => $this->getUserName(),
+            'email' => $inputs['email'],
+            'password' => Hash::make($inputs['password'])            
+        ]);
+
+        return response()->success(['message' => 'User registered successfully.', 'user' => $user]);
+    }
+
+    private function getUserName()
+    {
+        while(true) {
+
+            $random_num = rand(10000000, 99999999);
+
+            if(! User::where('user_name' ,$random_num)->exists()) break;
+        }
+ 
+        return $random_num;
     }
 }
