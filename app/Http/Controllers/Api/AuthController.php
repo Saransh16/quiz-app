3bc2fcc7278ecb2893dd6d22fa5377f8f1f891ff
Saransh Kumar <ksaransh682@gmail.com>
2022-12-18 01:00:54 +0530

    Add in register validator

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\RegisterRequest;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $inputs = $request->validated();
+
+        dd("here");
+    }
+}
